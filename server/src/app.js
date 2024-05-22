3a3683ca12cefdd4d9e9044ffdb5468dd2dfa88a
Kaiden Ong <kaiden.ong000@gmail.com>
2024-05-21 17:21:09 -0700

    Moved some backend stuff over

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 import receiptsRouter from '../routes/receipts.js';
+import pantryRouter from '../routes/pantry.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const port = 3000;
 const app = express();
@@ -10,17 +20,23 @@ app.use(cors());
 /** GET endpoint for sending back a Hello World message */
 app.get('/', (req, res) => {
   res.type('text');
-  res.send('Hello, World!');
+  res.send('Hello, World! This is the home page');
 });
 
-app.get('/hello', (req, res) => {
-  res.type('text');
-  res.send('Hello, World!');
-});
+// middleware 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+// routers
+// if working in backend, please add router then go to routes folder and add code there
 app.use('/receipts', receiptsRouter);
+app.use('/pantry', pantryRouter);
 
 // Tells our app to listen to the given port
+// for now we will run on port 3000
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
