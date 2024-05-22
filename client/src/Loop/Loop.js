3480da2e18c30b4c2d2ee77454a56ab7c764f76e
Juntab1 <jsbwhaleguy@gmail.com>
2024-05-22 00:23:14 -0700

    error with loading check boxes

--- client/src/Loop/Loop.js
+++ client/src/Loop/Loop.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import CheckBox from "./CheckBox/CheckBox";
+import React, {useState} from 'react';
+import CheckBox from "../CheckBox/CheckBox";
 
 
 // This is where we can create number of checkBox objects depending on the number passed into Hook
 const Loop = ({numberOfItems}) => {
     
     // initialize the state of each checkbox with useStae and Array.from
-    [used, setUsed] = useState(Array.from({length: numberOfItems}, () => false));
+    const [used, setUsed] = useState(Array.from({length: numberOfItems}, () => false));
     // HandleCheckBoxChange function to change the state when changed
     const HandleCheckBoxChange = (index) => {
         setUsed((previous) => 
