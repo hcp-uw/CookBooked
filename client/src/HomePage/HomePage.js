20e4afa33480d364a8b28c274890b9534c1046b1
Juntab1 <jsbwhaleguy@gmail.com>
2024-07-11 23:17:33 -0700

    updated with card

--- client/src/HomePage/HomePage.js
+++ client/src/HomePage/HomePage.js
@@ -1,94 +1,139 @@
 import * as React from 'react';
-import { Button, StyleSheet, Text, View } from "react-native";
-import { auth } from '../../firebase'
-import styles from './HomePage.style'
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons'
-import {useState, useEffect} from 'react';
+import styles from './HomePage.style';
+import { Text, StyleSheet, Button, View, Image, Modal, Pressable } from "react-native"; 
+import { TextInput } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons'; 
+import { COLORS } from '../constants';
+// Import the mainPageCard component where custom card components are stored.
+import MainPageCard from '../Common/Cards/mainPage/mainPageCard';
 
-const Tab = createBottomTabNavigator();
+
+
 
 const HomePage = ({ navigation }) => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(user => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(null);
-      }
-    });    
-    return unsubscribe;
-  }, []);
-
-  const handleButtonClick = () => {
-    console.log("Button clicked!");
-    navigation.navigate('ReceiptPage');
-    // You can perform any action you want here
-  };
-  const loginPage = () => {
-    navigation.navigate('LoginScreen');
-  };
-  const signupPage = () => {
-    navigation.navigate('SignupScreen');
-  };
-  const logout = async () => {
-    try {
-      await auth.signOut();
-      navigation.navigate('HomePage');
-      setUser(null);
-    } catch (error) {
-      console.error('Error signing out:', error);
-    }
-  };
-
   return (
     <View style={styles.container}>
-      <Text>CookBooked</Text>
-      {user ? (
-        <View>
-          <Text>Welcome, {user.email}</Text>
-          <Button onPress={logout} title="Logout" color="#F2555A" />
-          <Button onPress={handleButtonClick} title="Click me!" color="#F2555A" />
-        </View>
-      ) : (
-        <View>
-          <Button onPress={handleButtonClick} title="Click me!" color="#F2555A" />
-          <Button onPress={loginPage} title="Login!" color="#F2555A" />
-          <Button onPress={signupPage} title="Signup!" color="#F2555A" />
-        </View>
-      )}
-      {(
-      <NavigationContainer>
-        <Tab.Navigator
-          initialRouteName={'HomePage'}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              let rn = route.name;
-
-              if (rn === 'homePage') {
-                iconName = focused ? 'home' : 'home-outline'
-              }
-
-              return <Ionicons name={iconName} size={size} color={color}/>;
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'grey',
-            labelStyle: {paddingBottom: 10, fontSize: 10 },
-            style: {padding: 10, height: 70}
-          }}
-          >
-          <Tab.Screen name='LoginPage' component={loginPage}/>
-        </Tab.Navigator>
-      </NavigationContainer> 
-    )}
+      <Image
+        source={require('../../../client/assets/foodImg/CBTofuSalad.png')}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <View style={styles.searchBarContainer}>
+        <Icon name="search" size={30} color={COLORS.gray} style={styles.iconSearch}/>
+        <TextInput
+          underlineColor='transparent'
+          placeholder='Search Recipes'
+          placeholderTextColor={COLORS.gray2}
+          style={styles.searchBar}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>Your foodprints</Text>
+        <MainPageCard
+          // image={'../../../../client/assets/foodImg/CBTofuSalad.png'}
+          itemName={"Salad"}
+        />
+      </View>
     </View>
-  );
+  )
 }
 
 export default HomePage;
+// import * as React from 'react';
+// import { Button, StyleSheet, Text, View } from "react-native";
+// import { auth } from '../../firebase'
+// import styles from './HomePage.style'
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import Ionicons from 'react-native-vector-icons/Ionicons'
+// import {useState, useEffect} from 'react';
+
+// const HomePage = ({ navigation }) => {
+//   const [user, setUser] = useState(null);
+
+//   useEffect(() => {
+//     const unsubscribe = auth.onAuthStateChanged(user => {
+//       if (user) {
+//         setUser(user);
+//       } else {
+//         setUser(null);
+//       }
+//     });    
+//     return unsubscribe;
+//   }, []);
+
+//   const handleButtonClickReceipt = () => {
+//     console.log("Button clicked!");
+//     navigation.navigate('ReceiptPage');
+//     // You can perform any action you want here
+//   };
+
+//   const handleButtonClickTab = () => {
+//     console.log("Button clicked!");
+//     navigation.navigate('HomePage');
+//     // You can perform any action you want here
+// };
+//   const loginPage = () => {
+//     navigation.navigate('LoginScreen');
+//   };
+//   const signupPage = () => {
+//     navigation.navigate('SignupScreen');
+//   };
+//   const logout = async () => {
+//     try {
+//       await auth.signOut();
+//       navigation.navigate('HomePage');
+//       setUser(null);
+//     } catch (error) {
+//       console.error('Error signing out:', error);
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text>CookBooked</Text>
+//       {user ? (
+//         <View>
+//           <Text>Welcome, {user.email}</Text>
+//           <Button onPress={logout} title="Logout" color="#F2555A" />
+//           <Button onPress={handleButtonClickReceipt} title="Click me!" color="#F2555A" />
+//         </View>
+//       ) : (
+//         <View>
+//           <Button onPress={handleButtonClickReceipt} title="Click me!" color="#F2555A" />
+//           <Button onPress={loginPage} title="Login!" color="#F2555A" />
+//           <Button onPress={signupPage} title="Signup!" color="#F2555A" />
+//         </View>
+//       )}
+//       {(
+//       <NavigationContainer>
+//         <Tab.Navigator
+//           initialRouteName={'HomePage'}
+//           screenOptions={({ route }) => ({
+//             tabBarIcon: ({ focused, color, size }) => {
+//               let iconName;
+//               let rn = route.name;
+
+//               if (rn === 'homePage') {
+//                 iconName = focused ? 'home' : 'home-outline'
+//               }
+
+//               return <Ionicons name={iconName} size={size} color={color}/>;
+//             },
+//           })}
+//           tabBarOptions={{
+//             activeTintColor: 'tomato',
+//             inactiveTintColor: 'grey',
+//             labelStyle: {paddingBottom: 10, fontSize: 10 },
+//             style: {padding: 10, height: 70}
+//           }}
+//           >
+//           <Tab.Screen name='LoginPage' component={loginPage}/>
+//         </Tab.Navigator>
+//       </NavigationContainer> 
+//     )}
+//     </View>
+//   );
+// }
+
+// export default HomePage;
--- client/src/HomePage/HomePage.style.js
+++ client/src/HomePage/HomePage.style.js
@@ -1,12 +1,54 @@
 import { StyleSheet } from "react-native";
-
 import { COLORS, FONT, SIZES } from "../constants";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        // Change to 'center' to align items vertically at the center
         alignItems: 'center',
-        justifyContent: 'center',
+    },
+    image: {
+        width: '100%',
+        height: '40%',
+        borderRadius: 8,
+    },
+    searchBarContainer: {
+        width: '100%',
+        borderColor: '#fff',
+        marginTop: SIZES.xSmall,
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddinLeft: '4%',
+    },
+    searchBar: {
+        flex: 1,
+        height: '5%',
+        width: '85%',
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        paddingLeft: '8%',
+        borderRadius: 10,
+        backgroundColor: 'transparent',
+        fontSize: 24,
+        borderColor: COLORS.gray2,
+        borderWidth: 2.3,
+    },
+    iconSearch: {
+        position: 'absolute',
+        left: '4%',
+        // zIndex, the higher number lets it stack on top
+        zIndex: 1,
+        padding: '1%',
+    },
+    textContainer: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+    },
+    textStyle: {
+        fontFamily: FONT.bold,
+        fontSize: 24,
+        textAlign: 'left',
     }
 })
 
