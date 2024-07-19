8bbd59980b06ed6e0051d40b1ea9f67d1cb3e1c0
Juntaro Bruttomesso <1092410@apps.nsd.org>
2024-07-19 10:51:17 -0700

    created settingPage

---
+++ client/src/SettingPage/SettingPage.js
@@ -0,0 +1,17 @@
+// SettingPage.js
+// Able to change features of your account.
+
+// Import core React functionality from the React package.
+import React, { useState } from 'react';
+// Import specific components and utilities from React Native for building the user interface.
+import { Text, StyleSheet, Button, View, Image, Modal, Pressable } from "react-native"; 
+
+const SettingPage = ({ navigation }) => {
+    return (
+        <View>
+            <Text>SettingPage</Text>
+        </View>
+    )
+}
+
+export default SettingPage;
