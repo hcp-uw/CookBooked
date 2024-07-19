52c87e3d380f2c831a148fa4676bdfef1faa8f86
Juntaro Bruttomesso <1092410@apps.nsd.org>
2024-07-19 10:50:53 -0700

    SearchPage Created

---
+++ client/src/SearchPage/SearchPage.js
@@ -0,0 +1,18 @@
+// SearchPage.js
+// This page we can search for foods in our pantry
+// and we can also search for recepies.
+
+// Import core React functionality from the React package.
+import React, { useState } from 'react';
+// Import specific components and utilities from React Native for building the user interface.
+import { Text, StyleSheet, Button, View, Image, Modal, Pressable } from "react-native"; 
+
+const SearchPage = ({ navigation }) => {
+    return (
+        <View>
+            <Text>SeachPage</Text>
+        </View>
+    )
+}
+
+export default SearchPage;
