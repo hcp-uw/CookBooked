f212bcfddb8f3da8b24383163841eb3c74223a31
Juntaro Bruttomesso <1092410@apps.nsd.org>
2024-07-22 14:45:58 -0700

    no change

--- client/src/StartPage/RealStartPage.js
+++ client/src/StartPage/RealStartPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet} from 'react-native';
 import { COLORS } from '../constants' 
 import { CommonActions } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
